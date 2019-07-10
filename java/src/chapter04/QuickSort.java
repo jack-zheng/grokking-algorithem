fed96488c356de6eae3ba3db647fcc28e86ad7c3
jack-zheng <lanmolei0124@163.com>
2019-07-11 00:22:55 +0800

    finish java version quick sort

--- java/src/chapter04/QuickSort.java
+++ java/src/chapter04/QuickSort.java
@@ -2,16 +2,39 @@ package chapter04;
 
 public class QuickSort {
     public static void main(String[] args) {
-
+        int[] test = new int[]{4,6,2,8,0,1,3};
+        quickSort(test, 0, test.length-1);
+        for (int sub : test) {
+            System.out.print(sub + " ");
+        }
+    }
+    private static void quickSort(int[] arr, int left, int right) {
+        if (left > right)
+            return;
+        int pivot = partition(arr, left, right);
+        quickSort(arr, left, pivot-1);
+        quickSort(arr, pivot+1, right);
     }
 
-    private static int[] quickSort(int[] arr) {
-//        if (arr.length < 2)
-//            return arr;
-//        int pivot = arr[0];
-//        for (int sub : arr) {
-//            if (sub <= pivot)
-
-    return null;
+    /**
+     * About the partition, you can refer to this blog, the picture is nice for you to understand this algorithm.
+     *  https://www.cnblogs.com/MOBIN/p/4681369.html
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    private static int partition(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        while (left != right) {
+            while (arr[right] > pivot && left < right)
+                right--;
+            arr[left] = arr[right];
+            while (arr[left] < pivot && left < right)
+                left++;
+            arr[right] = arr[left];
+        }
+        arr[left] = pivot;
+        return left;
     }
 }
